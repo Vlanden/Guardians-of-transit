4d435005c4efcd094279b5bfd4112d0edf061810
Vlanden <99295243+Vlanden@users.noreply.github.com>
2025-04-15 19:51:44 -0600

    z

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from app.main import app  # Asegúrate de usar el nombre correcto si es diferente
+from app import create_app  # Asegúrate de usar el nombre correcto si es diferente
 
 if __name__ == "__main__":
     app.run()
