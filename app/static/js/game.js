e3f58ea849555340b44a98550ad5f3732060b7fe
Vlanden <99295243+Vlanden@users.noreply.github.com>
2025-05-07 20:00:08 -0600

    Otra vez el repaso
                    <!--<button id="btnRepasar" class="btn btn-outline-primary me-2">Repasar contenido</button>-->

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -215,7 +215,6 @@ function iniciarQuiz(datosJuego) {
               <p>${datosJuego.titulo}</p>
               <p>${datos.mensaje || ""}</p>
               <div class="mt-3">
-                <--!<button id="btnRepasar" class="btn btn-outline-primary me-2">Repasar contenido</button>-->
                 <button id="btnReiniciar" class="btn btn-outline-secondary me-2">Reiniciar quiz</button>
                 <button id="btnOtroQuiz" class="btn btn-outline-success">Hacer otro quiz</button>
               </div>
